492ca47497a981b6bfa0d6aeeedf44b0cd662c7d
anubha193 <rajputanubha1@gmail.com>
2023-07-31 17:11:34 +0530

    commit

---
+++ sliderdata.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Taaza Khabar',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://m.apnlive.com/sortd-service/imaginary/v22-01/jpg/large/high?url=YXBuLW5ld3MtcHJvZC1zb3J0ZC9tZWRpYTE0OTIyNGMwLTdhZDgtMTFlZC1hYzEzLTY1ZGYwYTU5YTUzNi5qcGc='
+    },
+    {
+        name: 'Barbie',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://static.euronews.com/articles/stories/07/51/45/54/1440x810_cmsv2_d794baf9-5913-5e00-ab52-461ab3b4b56f-7514554.jpg'
+    },
+    {
+        name: 'Kerala Story',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://images.mid-day.com/images/images/2023/apr/kerala-story_d.jpg'
+    },
+    {
+        name: 'BUILDING A BUILDING',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://m.media-amazon.com/images/M/MV5BNTM4NTg2N2QtNTUyNC00YmJhLTg5YWEtOGJlYjlmZDY2MDNkXkEyXkFqcGdeQXVyMTIwMjY0NjQz._V1_.jpg'
+    },
+    {
+        name: 'The Amazing Spider-Man',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://wallpaperaccess.com/full/291950.jpg'
+    }
+]
